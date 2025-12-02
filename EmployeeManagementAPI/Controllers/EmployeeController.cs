e5eaf95628a97cc1ea94810a4b659e6524fa441f
Konstantatou Nefeli <n.konstantatou@megasoft.gr>
2025-12-02 11:15:53 +0200

    Add project files.

---
+++ EmployeeManagementAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using EmployeeManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeManagementAPI.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return employees.Values;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Employee> GetById(int id)
+        {
+            return employees.TryGetValue(id, out var employee) ? Ok(employee) : NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<Employee> Add([FromBody] Employee value)
+        {
+            if((value.Salary <= 0) || value.Name.Equals("") || value.Position.Equals("") || value.Department.Equals(""))
+                return BadRequest();
+
+            value.Id = employees.Count() + 1;
+            return employees.TryAdd(value.Id, value) ? Created() : BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return employees.Remove(id) ? Ok() : NotFound();
+        }
+
+
+        [HttpPut]
+        public ActionResult Update([FromBody] Employee value)
+        {
+            if (employees.ContainsKey(value.Id))
+            {
+                employees[value.Id] = value;
+                return Ok(employees[value.Id]);
+            }
+            else return NotFound();
+        }
+
+        static private Dictionary<int, Models.Employee> employees = new();
+    }
+}
---
+++ EmployeeManagementAPI/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagementAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Position { get; set; }
+        public required string Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
